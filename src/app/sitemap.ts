68af076c41e71cf9c528812632da9a723d962501
vaishnaviyadav25 <vaishnaviyadav25march@gmail.com>
2025-12-08 15:32:12 +0530

    Add

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,42 +1,44 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const now = new Date();
+
   return [
     {
       url: "https://vaishi.vercel.app/",
-      lastModified: new Date().toISOString(),
+      lastModified: now,
     },
     {
       url: "https://vaishi.vercel.app/product",
-      lastModified: new Date().toISOString(),
+      lastModified: now,
     },
     {
       url: "https://vaishi.vercel.app/contact",
-      lastModified: new Date().toISOString(),
+      lastModified: now,
     },
     {
       url: "https://vaishi.vercel.app/cart",
-      lastModified: new Date().toISOString(),
+      lastModified: now,
     },
     {
       url: "https://vaishi.vercel.app/login",
-      lastModified: new Date().toISOString(),
+      lastModified: now,
     },
     {
       url: "https://vaishi.vercel.app/order",
-      lastModified: new Date().toISOString(),
+      lastModified: now,
     },
     {
       url: "https://vaishi.vercel.app/privacy-policy",
-      lastModified: new Date().toISOString(),
+      lastModified: now,
     },
     {
       url: "https://vaishi.vercel.app/terms-and-conditions",
-      lastModified: new Date().toISOString(),
+      lastModified: now,
     },
     {
       url: "https://vaishi.vercel.app/return-policy",
-      lastModified: new Date().toISOString(),
+      lastModified: now,
     },
   ];
 }
